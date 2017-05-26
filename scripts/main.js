ccac604df0160d0ad427eee5d2e525bbe2989059
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-05-26 17:58:14 -0400

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,28 @@
 console.log("hey?")
 
+var randomResult= Math.floor((Math.random() * 10) + 1);
+console.log(randomResult)
 
-function makeSprinkles(){
-	var x= document.getElementById("sprinkles")
+function changeColor(){
+	var x= document.getElementById("blankbutton")
 
-	if (x.style.display==="none"){
-		x.style.display= 'block';
+	if (randomResult >=8){
+		x.style.backgroundColor="green"
 	} else {
-		x.style.display='none';
+		x.style.backgroundColor="red"
 	}
+
 }
 
+
+// <a class="test" name="Name 1"></a>
+// <a class="test" name="Name 2"></a>
+// <a class="test" name="Name 3"></a>
+//
+// // javascript
+// var elements = document.getElementsByClassName("test");
+// var names = '';
+// for(var i=0; i<elements.length; i++) {
+//     names += elements[i].name;
+// }
+// document.write(names);
