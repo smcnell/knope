f5a06b10f003301134f0971bcfe026851d066f23
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-05-26 16:19:11 -0400

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+console.log("hey?")
+
+
+function makeSprinkles(){
+	document.getElementById(".btn").toggleClass('show');
+}
+
+// $('.btn').click(function(){
+//   $('.sprinkles').toggleClass('show');
+// })
+
+// button.onClick= function(){
+// 	$('.sprinkles').toggleClass('show');
+// }
