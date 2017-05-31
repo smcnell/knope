44d8f6ca1d8bb5cf518c9e5caea29331ccf1e23f
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-05-31 14:50:03 -0400

    random working

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,37 @@
 console.log("hey?")
 
-var randomResult= Math.floor((Math.random() * 10) + 1);
-console.log(randomResult)
 
-function changeColor(){
-	var x= document.getElementById("blankbutton")
 
-	if (randomResult >=8){
-		x.style.backgroundColor="green"
-	} else {
-		x.style.backgroundColor="red"
-	}
+
+
+
+var randomOne=""
+function randomQuote(){
+	var quotesArr= [
+		{text: "Hey you."},
+		{text: "What's up"},
+		{text: "Your mom"},
+		{text: "Hey hey heyyy"}
+	]
+
+	// console.log(quotesArr)
+	randomOne= quotesArr[Math.floor(Math.random() * quotesArr.length)]
+	console.log(randomOne)
 
 }
 
+function makeNewQuote(){
+	console.log(randomOne)
 
-// <a class="test" name="Name 1"></a>
-// <a class="test" name="Name 2"></a>
-// <a class="test" name="Name 3"></a>
-//
-// // javascript
-// var elements = document.getElementsByClassName("test");
-// var names = '';
-// for(var i=0; i<elements.length; i++) {
-//     names += elements[i].name;
-// }
-// document.write(names);
+
+	var myButton= document.getElementById("quoteButton");
+	var myQuote= document.getElementById("quote").innerHTML="hey dear"
+
+	console.log(myQuote)
+
+
+	console.log("working")
+
+
+
+}
