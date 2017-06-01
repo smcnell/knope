5e068dd76cea476c0dd903b98ffb29322f92f5ce
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-06-01 14:10:01 -0400

    random working

--- scripts/main.js
+++ scripts/main.js
@@ -6,31 +6,48 @@ console.log("hey?")
 
 
 var randomOne=""
-function randomQuote(){
-	var quotesArr= [
-		{text: "Hey you."},
-		{text: "What's up"},
-		{text: "Your mom"},
-		{text: "Hey hey heyyy"}
-	]
-
-	// console.log(quotesArr)
-	randomOne= quotesArr[Math.floor(Math.random() * quotesArr.length)]
-	console.log(randomOne)
-
-}
+// function randomQuote(){
+// 	var quotesArr= [
+// 		{text: "The mind is just like a muscle - the more you exercise it, the stronger it gets and the more it can expand."},
+// 		{text: "Fixed mindset worries in the nest and the growth mindset dances on the edge."},
+// 		{text: "Becoming is better than being"},
+// 		{text: "Picture your brain forming new connections as you meet the challenge and learn. Keep on going."}
+// 	]
+//
+// 	// console.log(quotesArr)
+// 	randomOne= quotesArr[Math.floor(Math.random() * quotesArr.length)]
+// 	console.log(randomOne)
+//
+// }
 
 function makeNewQuote(){
+
+	function randomQuote(){
+		var quotesArr= [
+			{text: "The mind is just like a muscle - the more you exercise it, the stronger it gets and the more it can expand."},
+			{text: "Fixed mindset worries in the nest and the growth mindset dances on the edge."},
+			{text: "Becoming is better than being"},
+			{text: "Picture your brain forming new connections as you meet the challenge and learn. Keep on going."}
+		]
+
+		// console.log(quotesArr)
+		randomOne= quotesArr[Math.floor(Math.random() * quotesArr.length)]
+		console.log(randomOne)
+
+	}
+
+
+
 	console.log(randomOne)
 
 
 	var myButton= document.getElementById("quoteButton");
-	var myQuote= document.getElementById("quote").innerHTML="hey dear"
+	var myQuoteInner= document.getElementById("quote")
+	myQuoteInner.innerHTML=randomOne.text
 
-	console.log(myQuote)
+	randomQuote()
 
 
-	console.log("working")
 
 
 
