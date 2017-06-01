7dd97731b47dd8a8642536d57f3c4af1c78032b9
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-06-01 14:35:18 -0400

    new quotes

--- scripts/main.js
+++ scripts/main.js
@@ -6,19 +6,7 @@ console.log("hey?")
 
 
 var randomOne=""
-// function randomQuote(){
-// 	var quotesArr= [
-// 		{text: "The mind is just like a muscle - the more you exercise it, the stronger it gets and the more it can expand."},
-// 		{text: "Fixed mindset worries in the nest and the growth mindset dances on the edge."},
-// 		{text: "Becoming is better than being"},
-// 		{text: "Picture your brain forming new connections as you meet the challenge and learn. Keep on going."}
-// 	]
-//
-// 	// console.log(quotesArr)
-// 	randomOne= quotesArr[Math.floor(Math.random() * quotesArr.length)]
-// 	console.log(randomOne)
-//
-// }
+
 
 function makeNewQuote(){
 
@@ -27,7 +15,15 @@ function makeNewQuote(){
 			{text: "The mind is just like a muscle - the more you exercise it, the stronger it gets and the more it can expand."},
 			{text: "Fixed mindset worries in the nest and the growth mindset dances on the edge."},
 			{text: "Becoming is better than being."},
-			{text: "Picture your brain forming new connections as you meet the challenge and learn. Keep on going."}
+			{text: "Picture your brain forming new connections as you meet the challenge and learn. Keep on going."},
+			{text: "Do not judge me by my successess, judge me by how many times I fell down and got back up again."},
+			{text: "It is not that I am so smart, it is just that I stay with problems longer."},
+			{text: "To avoid criticism, do nothing, say nothing, be nothing."},
+			{text: "You've only failed if you've given up."
+
+
+
+			}
 		]
 
 		// console.log(quotesArr)
