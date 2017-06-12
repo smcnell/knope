2281f65d0a97db565c7295380826f67ca1d0777b
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-06-11 22:40:31 -0400

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -12,14 +12,58 @@ function makeNewQuote(){
 
 	function randomQuote(){
 		var quotesArr= [
-			{text: "The mind is just like a muscle - the more you exercise it, the stronger it gets and the more it can expand."},
-			{text: "Fixed mindset worries in the nest and the growth mindset dances on the edge."},
-			{text: "Becoming is better than being."},
-			{text: "Picture your brain forming new connections as you meet the challenge and learn. Keep on going."},
-			{text: "Do not judge me by my successess, judge me by how many times I fell down and got back up again."},
-			{text: "It is not that I am so smart, it is just that I stay with problems longer."},
-			{text: "To avoid criticism, do nothing, say nothing, be nothing."},
-			{text: "You've only failed if you've given up."
+			{text: "sweet"},
+			{text: "innocent"},
+			{text: "pretty"},
+			{text: "beautiful"},
+			{text: "cool"},
+			{text: "talented"},
+			{text: "powerful"},
+			{text: "worthy"},
+			{text: "amazing"},
+			{text: "cunning"},
+			{text: "pliable"},
+			{text: "genius"},
+			{text: "sophisticated"},
+			{text: "naive"},
+			{text: "poetic"},
+			{text: "brilliant"
+
+
+
+			}
+		]
+
+
+		var quotesArr2= [
+			{text: "tropical"},
+			{text: "noble"},
+			{text: "rule-breaking"},
+			{text: "thoughtful"},
+			{text: "stupid-hot"},
+			{text: "unicorn"},
+			{text: "multi-ethnic"},
+			{text: "doll-footed"},
+			{text: "glowing"},
+			{text: "powerful"
+
+
+
+			}
+		]
+
+		var quotesArr3= [
+			{text: "fish"},
+			{text: "ox"},
+			{text: "spinster"},
+			{text: "moth"},
+			{text: "heifer"},
+			{text: "sunfish"},
+			{text: "brain"},
+			{text: "sun-goddess"},
+			{text: "land-mermaid"},
+
+			{text: "newborn baby"
 
 
 
@@ -30,16 +74,20 @@ function makeNewQuote(){
 		randomOne= quotesArr[Math.floor(Math.random() * quotesArr.length)]
 		console.log(randomOne)
 
+		randomTwo= quotesArr2[Math.floor(Math.random() * quotesArr2.length)]
+		console.log(randomTwo)
+
+		randomThree= quotesArr3[Math.floor(Math.random() * quotesArr3.length)]
+		console.log(randomThree)
+
 	}
 
 
 
-	console.log(randomOne)
-
 randomQuote()
 	var myButton= document.getElementById("quoteButton");
 	var myQuoteInner= document.getElementById("quote")
-	myQuoteInner.innerHTML='"' + randomOne.text + '"'
+	myQuoteInner.innerHTML= randomOne.text + " "+ randomTwo.text + " "+ randomThree.text
 
 
 
